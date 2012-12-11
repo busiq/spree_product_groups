3b53c60dd672d855c11794942c88e9e5cec0bc5f
busiq <mbusiq@gmail.com>
2012-12-11 13:04:27 +0100

    Small changes in validation and preferences

--- app/models/spree/promotion/rules/product_decorator.rb
+++ app/models/spree/promotion/rules/product_decorator.rb
@@ -1,13 +1,22 @@
 Spree::Promotion::Rules::Product.class_eval do
   belongs_to :product_group
+  preference :group, :boolean
+  attr_accessible :preferred_group
 
   def eligible_products
-    product_group ? product_group.products : products
+    preferred_group ? product_group.products : products
   end
 
-  def products_source=(source)
-    if source.to_s == 'manual'
-      self.product_group_id = nil
+  private
+  def only_one_promotion_per_product
+    if preferred_group
+      if Spree::Promotion::Rules::Product.where("spree_promotion_rules.id != ?", self.id).where(product_group_id: self.product_group_id).present?
+        errors[:base] << "You can't create two promotions for the same product"
+      end
+    else
+      if Spree::Promotion::Rules::Product.where("spree_promotion_rules.id != ?", self.id).all.map(&:products).flatten.uniq!.include?(products)
+        errors[:base] << "You can't create two promotions for the same product"
+      end
     end
   end
 
